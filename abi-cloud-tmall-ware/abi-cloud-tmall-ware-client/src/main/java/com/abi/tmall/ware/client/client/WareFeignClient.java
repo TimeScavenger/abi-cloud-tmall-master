cdea12f2538b35c7aaa1ac865c7a3cc0d43bf428
YunleiSun <v_YunleiSun@budweiserapac.com>
2022-04-10 21:45:28 +0800

    仓库服务：删除Feign接口中的无用代码

--- abi-cloud-tmall-ware/abi-cloud-tmall-ware-client/src/main/java/com/abi/tmall/ware/client/client/WareFeignClient.java
+++ abi-cloud-tmall-ware/abi-cloud-tmall-ware-client/src/main/java/com/abi/tmall/ware/client/client/WareFeignClient.java
@@ -22,14 +22,10 @@ import java.util.List;
 public interface WareFeignClient {
 
     /**
-     * SpringCloud远程调用原理：WareFeignService.saveSpuBounds(spuBoundsTo);
-     * 1、@RequestBody将这个对象转为json。
-     * 2、找到tmall-cloud-coupon服务，给/coupon/console/spubounds/save发送请求。将上一步转的json放在请求体位置，发送请求。
-     * 3、对方服务收到请求。请求体里有json数据。(@RequestBody SpuBoundsEntity spuBounds)；将请求体的json转为SpuBoundsEntity；
-     * 总结：只要json数据模型是兼容的。双方服务无需使用同一个to
+     * 查询 Sku是否有库存
      *
-     * @param wareStockDto
-     * @return
+     * @param wareStockReq SkuCode列表
+     * @return 商品库存数列表
      */
     @PostMapping(value = "/console/ware-sku-relation/hasStock")
     public ApiResponse<List<WareSkuRelationStockResp>> querySkuHasStock(@RequestBody WareStockReq wareStockReq);

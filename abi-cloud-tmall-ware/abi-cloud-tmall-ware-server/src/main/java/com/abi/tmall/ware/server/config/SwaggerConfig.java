e75152124742b39acecaa736a4d2ed0e16d4c3eb
YunleiSun <v_YunleiSun@budweiserapac.com>
2022-04-25 22:23:15 +0800

    仓库服务：swagger添加公共的请求参数

--- abi-cloud-tmall-ware/abi-cloud-tmall-ware-server/src/main/java/com/abi/tmall/ware/server/config/SwaggerConfig.java
+++ abi-cloud-tmall-ware/abi-cloud-tmall-ware-server/src/main/java/com/abi/tmall/ware/server/config/SwaggerConfig.java
@@ -6,11 +6,18 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Swagger 配置文件
@@ -41,6 +48,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .enable(flag)
+                .globalRequestParameters(getGlobalRequestParameters())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.abi.tmall.ware.server.controller"))
                 .build();
@@ -60,5 +68,31 @@ public class SwaggerConfig {
                 .contact(new Contact("", "", ""))
                 .version("1.0")
                 .build();
+    }
+
+    /**
+     * 生成全局通用参数
+     *
+     * @return
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("abiOneId")
+                .description("abiOneId")
+                .required(false)
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+        parameters.add(new RequestParameterBuilder()
+                .name("userType")
+                .description("userType")
+                .required(false)
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+        return parameters;
     }
 }
